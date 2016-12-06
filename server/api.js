40e9eff5266385f96d626d1a694940718bff22f2
ajay <ajaysunarthi@iitj.ac.in>
2016-12-06 13:29:15 +0530

    Server setup

--- server/api.js
+++ server/api.js
@@ -2,27 +2,72 @@ var express = require('express');
 
 var router = express.Router();
 
-var Card = require('./models.js');
+var Card = require('./model.js');
 
-router.use(function(req, res, next) {
-    console.log('Some request is used !');
-    next();
-});
+// router.use(function(req, res, next) {
+//     console.log('Some request is used !');
+//     next();
+// });
 
 router.route('/cards')
 
-.post(function(req, res) {
+	.post(function(req, res) {
+        var card = new Card();
+        card.title = req.body.title;
+        card.description = req.body.description;
+        card.code = req.body.code;
+
+        card.save(function(err) {
+            if (err)
+                res.send(err);
+            res.json({ message: 'Card created!' });
+        });
 
     })
     .get(function(req, res) {
+    	Card.find(function(err, cards) {
+			if (err)
+				res.send(err);
 
+			res.json(cards);
+		});
     });
 
 router.route('/cards/:card_id')
 
-.post(function(req, res) {
-
-    })
     .get(function(req, res) {
+    	Card.findById(req.params.card_id, function(err, card) {
+			if (err)
+				res.send(err);
+			res.json(card);
+		});
+    })
 
-    });
+    .put(function(req, res) {
+		Card.findById(req.params.card_id, function(err, card) {
+
+			if (err)
+				res.send(err);
+
+			card.name = req.body.name;
+			card.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'card updated!' });
+			});
+
+		});
+	})
+	.delete(function(req, res) {
+		Card.remove({
+			_id: req.params.card_id
+		}, function(err, card) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted card' });
+		});
+	});
+
+module.exports = router;
