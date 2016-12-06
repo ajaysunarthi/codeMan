34873ce61a3ce3a76c0759d4bc6431e5ac1431a0
ajay <ajaysunarthi@iitj.ac.in>
2016-12-06 19:33:38 +0530

    api check

--- utils/api.js
+++ utils/api.js
@@ -11,7 +11,7 @@ var Card = require('./model.js');
 
 router.route('/cards')
 
-	.post(function(req, res) {
+.post(function(req, res) {
         var card = new Card();
         card.title = req.body.title;
         card.description = req.body.description;
@@ -25,49 +25,50 @@ router.route('/cards')
 
     })
     .get(function(req, res) {
-    	Card.find(function(err, cards) {
-			if (err)
-				res.send(err);
-
-			res.json(cards);
-		});
+        Card.find(function(err, cards) {
+            if (err)
+                res.send(err);
+            res.json(cards);
+        });
     });
 
 router.route('/cards/:card_id')
 
-    .get(function(req, res) {
-    	Card.findById(req.params.card_id, function(err, card) {
-			if (err)
-				res.send(err);
-			res.json(card);
-		});
+.get(function(req, res) {
+    Card.findById(req.params.card_id, function(err, card) {
+        if (err)
+            res.send(err);
+        res.json(card);
+    });
+})
+
+.put(function(req, res) {
+        Card.findById(req.params.card_id, function(err, card) {
+
+            if (err)
+                res.send(err);
+
+            card.title = req.body.title;
+            card.description = req.body.description;
+        	card.code = req.body.code;
+            card.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json(card);
+            });
+
+        });
     })
+    .delete(function(req, res) {
+        Card.remove({
+            _id: req.params.card_id
+        }, function(err, card) {
+            if (err)
+                res.send(err);
 
-    .put(function(req, res) {
-		Card.findById(req.params.card_id, function(err, card) {
-
-			if (err)
-				res.send(err);
-
-			card.name = req.body.name;
-			card.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'card updated!' });
-			});
-
-		});
-	})
-	.delete(function(req, res) {
-		Card.remove({
-			_id: req.params.card_id
-		}, function(err, card) {
-			if (err)
-				res.send(err);
-
-			res.json({ message: 'Successfully deleted card' });
-		});
-	});
+            res.json({ message: 'Successfully deleted card' });
+        });
+    });
 
 module.exports = router;
